09f7617a1119687ce7866715319ef4cee0fdce6d
Orrin Sheppard <shep2180@gmail.com>
2020-07-21 22:51:35 -0500

    First commit

---
+++ Lambda/app.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('VisitorCounterDB')
+
+def addVisitor():
+    resp = table.get_item(Key={'id': 'counter'})
+    value = resp['Item']['counter_value'] + 1
+    resp = table.update_item(Key={'id':'counter'}, UpdateExpression='set counter_value=:value', ExpressionAttributeValues={':value': value})
+
+def lambda_handler(event, context):
+    
+    addVisitor()
+    
+    resp2 = table.get_item(Key={'id':'counter'})
+    
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        "body": json.dumps({
+            "visitorCount": int(resp2["Item"]['counter_value'])
+        })
+    }
+    
+    return response
